f6286f95da0bd93b4ecb6f1f6f115cc93303f833
Pinyituri1 <jfernandotrujillo@ucundinamarca.edu.co>
2025-02-18 16:45:15 -0500

    3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola juan");
+
+
+        System.out.println("Hola fernando, ¿Como estas?");
     }
 }
